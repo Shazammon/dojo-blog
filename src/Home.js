5354caa9293fb83b84a269a71e6cd3a885fab4a0
Shazammon <ammonorgill@gmail.com>
2023-02-07 18:36:30 -0700

    set up onClick events for two buttons with anonymous functions and wrote two arrow functions for the onClick events

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,20 @@
 import react from 'react';
 
 export default function Home() {
+
+    const handleClick = (e) => {
+        console.log('Hello ninjas', e)
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello' + name)
+    }
+
     return (
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Ammon', e)}>Click me again</button>
         </div>
     )
 }
